a31ecdc0e1b7f618573f296c48c0bc295018eef9
DESKTOP-NLJQJ2E\Albert <albert.benavent.cabrera@gmail.com>
2023-05-26 07:15:22 +0200

    fix: fixed functions to send id to server

--- client/src/pages/drinks/tabs/config-tab-drinks/card-config-tab.tsx
+++ client/src/pages/drinks/tabs/config-tab-drinks/card-config-tab.tsx
@@ -3,78 +3,187 @@ import {
   Button,
   Card,
   CardContent,
-  TextField,
+  Slider,
   Typography,
 } from "@mui/material";
 import { ReactComponent as IcoWaterOn } from "../../../../icons/waterOn.svg";
 import { ReactComponent as IcoWaterOff } from "../../../../icons/waterOff.svg";
+import { useState } from "react";
+const color = "#009688";
 
 interface CardConfigTabProps {
-  title: string;
-  formData: {
-    pwm: number;
-    statePump: string;
+  card: {
+    id: number;
+    title: string;
   };
-  onChangeStatePumpButton: () => Promise<void>;
-  onChangePWM: (pwmTmp: string) => void;
-}
-export const CardConfigTab = (props: CardConfigTabProps) => {
-  const { formData, onChangePWM, onChangeStatePumpButton, title } = props;
 
-  const sendFormData = () => {};
+  sendFormDataServer: (
+    data: {
+      pwm: number;
+      timeCalibration: number;
+    },
+    id: number
+  ) => void;
+}
+
+interface SliderComponentProps {
+  onChangeValue: (pwmTmp: number) => void;
+}
+
+export const SliderComponent = (props: SliderComponentProps) => {
+  const [value, setValue] = useState(30 as number | number[]);
+  const { onChangeValue } = props;
+  const marks = [
+    {
+      value: 0,
+      label: "OFF",
+    },
+    {
+      value: 255,
+      label: "On",
+    },
+  ];
+
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue);
+    onChangeValue(newValue as number);
+  };
+
+  return (
+    <Box sx={{ padding: "1em", marginTop: "1em", width: "100%" }}>
+      <Slider
+        onChange={handleSliderChange}
+        valueLabelDisplay="on"
+        marks={marks}
+        max={255}
+        sx={{
+          "& .MuiSlider-thumb": {
+            color: color,
+          },
+          "& .MuiSlider-track": {
+            color: color,
+          },
+          "& .MuiSlider-rail": {
+            color: "#acc4e4",
+          },
+          "& .MuiSlider-active": {
+            color: "green",
+          },
+        }}
+        value={typeof value === "number" ? value : 0}
+        aria-label="Disabled slider"
+      />
+    </Box>
+  );
+};
+
+export const CardConfigTab = (props: CardConfigTabProps) => {
+  const { sendFormDataServer, card } = props;
+  const [cardForm, setCarForm] = useState({ pwm: 0, timeCalibration: 0 });
+
+
+  const onChangeBinaryValue = () => {
+    if (cardForm.pwm > 0) {
+      setCarForm({ ...cardForm, pwm: 0 });
+      sendFormDataServer(
+        {
+          ...cardForm,
+          pwm: 0,
+        },
+        card.id
+      );
+    } else {
+      setCarForm({ ...cardForm, pwm: 255 });
+      sendFormDataServer(
+        {
+          ...cardForm,
+          pwm: 255,
+        },
+        card.id
+      );
+    }
+  };
+
+  const onChangePwmValue = (pwmTmp: number) => {
+    setCarForm({ ...cardForm, pwm: pwmTmp });
+  };
+  const onChangeTimeCalibrationValue = (timeCalibrationTmp: number) => {
+    setCarForm({ ...cardForm, timeCalibration: timeCalibrationTmp });
+  };
+
+  const sendFormData = () => {
+    sendFormDataServer(cardForm, card.id);
+  };
+
   return (
     <Card>
       <CardContent>
-        <Box sx={{ display: "flex", alignItems: "baseline" }}>
-          {formData.statePump === "ON" ? (
-            <IcoWaterOn className="buttonsvg" style={{ fill: "black" }} />
-          ) : (
-            <IcoWaterOff className="buttonsvg " style={{ fill: "black" }} />
-          )}
-          <Typography variant="h6" gutterBottom>
-            {title}
-          </Typography>
-        </Box>
         <Box
           sx={{
             display: "flex",
-            width: "100%",
+            alignItems: "baseline",
             justifyContent: "space-between",
-            marginBottom: "1em",
           }}
         >
           <Typography variant="h6" gutterBottom>
-            {formData.statePump}
+            {card.title}
           </Typography>
           <Button
-            sx={{ backgroundColor: "#009688" }}
+            sx={{ backgroundColor: color }}
             variant="contained"
-            onClick={onChangeStatePumpButton}
+            onClick={onChangeBinaryValue}
           >
-            {formData.statePump === "ON" ? "PAUSE" : "ON"}
+            {cardForm.pwm === 0 ? (
+              <>
+                {" "}
+                PAUSE{" "}
+                <IcoWaterOff className="buttonsvg" style={{ fill: "black" }} />
+              </>
+            ) : (
+              <>
+                {" "}
+                ON{" "}
+                <IcoWaterOn className="buttonsvg" style={{ fill: "black" }} />
+              </>
+            )}
           </Button>
         </Box>
+        <Typography
+          variant="subtitle2"
+          gutterBottom={false}
+          sx={{ fontWeight: "bold", color: color }}
+        >
+          {"PWM:"}
+        </Typography>
         <Box
           sx={{
             display: "flex",
             width: "100%",
             justifyContent: "space-between",
-            marginBottom: "1em",
+            alignItems: "center",
           }}
         >
-          <TextField
-            fullWidth={false}
-            label={"Water pump pwm"}
-            value={formData.pwm}
-            type={"number"}
-            placeholder={"Nombre"}
-            variant={"standard"}
-            onChange={(
-              e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-            ) => {
-              onChangePWM(e.currentTarget.value);
-            }}
-          />
+          <SliderComponent onChangeValue={onChangePwmValue} />
+        </Box>
+        <Typography
+          variant="subtitle2"
+          gutterBottom={false}
+          sx={{ fontWeight: "bold", color: color }}
+        >
+          {"Time Calibration:"}
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+
+            alignItems: "center",
+          }}
+        >
+          <SliderComponent onChangeValue={onChangeTimeCalibrationValue} />
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
           <Button
             sx={{ backgroundColor: "#009688" }}
             variant="contained"
--- client/src/pages/drinks/tabs/config-tab-drinks/config-tab-drinks.tsx
+++ client/src/pages/drinks/tabs/config-tab-drinks/config-tab-drinks.tsx
@@ -2,7 +2,7 @@ import { Grid, Paper, Typography } from "@mui/material";
 import { useState } from "react";
 import { plantaService } from "../../../../services/PlantaController.service";
 
-import { CardConfigTab } from "./card-config-tab";
+import { CardConfigTab, SliderComponent } from "./card-config-tab";
 
 const waterPumps = [
   { id: 1, title: "Water pump 1" },
@@ -12,23 +12,18 @@ const waterPumps = [
 ];
 
 export const ConfigTabDrinks = () => {
-  const [formData, setformData] = useState({
-    pwm: 0,
-    statePump: "OFF",
-  });
 
-  const onChangePWM = (pwmTmp: string) => {
-    setformData({ ...formData, pwm: parseInt(pwmTmp) });
-  };
 
-  const onChangeStatePumpButton = async () => {
+  const sendFormDataServer = async (
+    data: {  pwm: number;
+      timeCalibration: number },
+    id: number
+  ) => {
+    console.log(data,id)
     const responseStateServer = await plantaService.postWaterPump1OnOFF();
-    if (formData.statePump === "OFF" && responseStateServer === "OFF") {
-      setformData({ ...formData, statePump: "ON", pwm: 255 });
-    } else {
-      setformData({ ...formData, statePump: "OFF", pwm: 0 });
-    }
+
   };
+
 
   return (
     <>
@@ -40,10 +35,8 @@ export const ConfigTabDrinks = () => {
           {waterPumps.map((card) => (
             <Grid key={card.id} item xs={12} sm={6} md={4}>
               <CardConfigTab
-                onChangePWM={onChangePWM}
-                formData={formData}
-                onChangeStatePumpButton={onChangeStatePumpButton}
-                title={card.title}
+                sendFormDataServer={sendFormDataServer}
+                card={card}
               />
             </Grid>
           ))}
